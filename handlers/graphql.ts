7aff51d4008b74f28ea8bc82f08680378af8410a
Chester ONeill <coneill@relaynetwork.com>
2020-09-17 16:20:53 -0400

    Call actual services to get product and client info

--- handlers/graphql.ts
+++ handlers/graphql.ts
@@ -4,12 +4,29 @@ const { ApolloServer, gql } = require("apollo-server-lambda");
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
+    client(id: String): Client!
     message(id: String): Message
+    productGroup(id: String): ProductGroup!
+  }
+
+  type Client {
+    id: String
+    branding: Branding!
+    productGroup(id: String): ProductGroup!
+  }
+
+  type Branding {
+    banner_s3_url: String!
+    icon_s3_url: String!
   }
 
   type Message {
     cas: String
     content: String
+  }
+
+  type ProductGroup {
+    name: String
   }
 `;
 
@@ -22,12 +39,35 @@ const resolvers = {
       );
       return response.data.message;
     },
+    client: async (_, args, { headers }) => {
+      const response = await axios.get(
+        `http://config-service-api-dev.relaystaging.com:8020/platform/v3/web/app/config/client/${args.id}`,
+        { headers }
+      );
+      return response.data.client;
+    },
+  },
+  Client: {
+    productGroup: async (parent, args, { headers }) => {
+      const response = await axios.get(
+        `http://config-service-api-dev.relaystaging.com:8020/platform/v3/web/app/config/client/${parent.id}/product_group/${args.id}`,
+        { headers }
+      );
+      return response.data.product_group;
+    },
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({
+    event: {
+      headers: { session: cookie },
+    },
+  }) => {
+    return { headers: { cookie } };
+  },
   playground: {
     endpoint: "/dev/graphql",
   },
